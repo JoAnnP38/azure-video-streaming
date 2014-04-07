b6fb929c9f40f52c3761c4496b4d6d3fd6ee17fc
Petar Kovačević <simunic.petar@gmail.com>
2014-04-07 13:57:59 +0200

    Added dummy controllers

---
+++ AzureVideoStreaming.Web/Controllers/VideoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AzureVideoStreaming.Model;
+
+namespace AzureVideoStreaming.Web.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Get(string videoId)
+        {
+            return Json(new Video("author", "title", "desc", "mp4", "vc1", "thumb", DateTime.Now));
+        }
+
+        public ActionResult GetAll()
+        {
+            return Json(new List<Video>(){new Video("author", "title", "desc", "mp4", "vc1", "thumb", DateTime.Now), new Video("author", "title", "desc", "mp4", "vc1", "thumb", DateTime.Now)});
+        }
+
+        public ActionResult GetLikes(string videoId)
+        {
+            return Json(new {Count = 4, LikedByCurrentUser = true});
+        }
+	}
+}
