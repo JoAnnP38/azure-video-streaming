35237cb7315fe0e1bdbb8571111f31f3d7810726
Petar Kovačević <simunic.petar@gmail.com>
2014-04-07 15:06:07 +0200

    Implemented video repositories

---
+++ AzureVideoStreaming.Core/VideoEncodingQueueRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzureVideoStreaming.Model;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureVideoStreaming.Core
+{
+    public class VideoEncodingQueueRepository
+    {
+        private CloudStorageAccount _storageAccount;
+
+        public VideoEncodingQueueRepository()
+        {
+            // Retrieve the storage account from the connection string.
+           _storageAccount = CloudStorageAccount.Parse(
+               CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            var client = _storageAccount.CreateCloudTableClient();
+            var table = client.GetTableReference(TableStorageConstants.VideoEncodingQueueTableKey);
+            table.CreateIfNotExists();
+
+        }
+
+
+        public VideoEncodingQueue Add(VideoEncodingQueue queue)
+        {
+            var client = _storageAccount.CreateCloudTableClient();
+            var table = client.GetTableReference(TableStorageConstants.VideoEncodingQueueTableKey);
+
+            var insertOperation = TableOperation.Insert(queue);
+            table.Execute(insertOperation);
+
+            return queue;
+        }
+
+      
+
+        public IList<VideoEncodingQueue> GetAll()
+        {
+            var client = _storageAccount.CreateCloudTableClient();
+            var table = client.GetTableReference(TableStorageConstants.VideoEncodingQueueTableKey);
+
+            TableQuery<VideoEncodingQueue> query = new TableQuery<VideoEncodingQueue>();
+            return table.ExecuteQuery(query).ToList();
+
+        } 
+    }
+}
